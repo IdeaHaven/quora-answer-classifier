54a24abda595d2134b8b8e6053b941434ec41941
EpiphanyMachine <EpiphanyMachine@gmail.com>
2013-07-19 17:33:03 -0700

    working on data formatting

--- yolo.py
+++ yolo.py
@@ -2,9 +2,13 @@
 """
 Created on Fri Jul 19 16:42:20 2013
 
-@author: gregory
+@author: Bit Shift Boys
 """
 
 import pandas as pd
-train = pd.read_csv('./input00.txt', sep="\s", skiprows=1,nrows=4500, names=["id","rank","col1","col2","col3","col4","col5","col6","col7","col8","col9","col10","col11","col12","col13","col14","col15","col16","col17","col18","col19","col20","col21","co22","col23"])
-test = pd.read_csv('./input00.txt', sep="\s", skiprows=4502, names=["id","col1","col2","col3","col4","col5","col6","col7","col8","col9","col10","col11","col12","col13","col14","col15","col16","col17","col18","col19","col20","col21","co22","col23"])
+# import rows 1-4501 and add column titles
+traintemp = pd.read_csv('./input00.txt', sep="\s", skiprows=1,nrows=4500, names=["id","rank","col1","col2","col3","col4","col5","col6","col7","col8","col9","col10","col11","col12","col13","col14","col15","col16","col17","col18","col19","col20","col21","col22","col23"])
+# import rows 4502-6002 and add column titles
+testtemp = pd.read_csv('./input00.txt', sep="\s", skiprows=4502, names=["id","col1","col2","col3","col4","col5","col6","col7","col8","col9","col10","col11","col12","col13","col14","col15","col16","col17","col18","col19","col20","col21","col22","col23"])
+# for columns slice out col id and :
+# 1-9 remove "n:" 10-23 remove "nn:"
