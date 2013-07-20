98a3808c27e5a132658675d7156a74ebaa96b037
EpiphanyMachine <EpiphanyMachine@gmail.com>
2013-07-19 19:26:21 -0700

    changed columns headers to numbers, removed col index from values, changed -1 to 0 in ratings

--- yolo.py
+++ yolo.py
@@ -7,8 +7,26 @@ Created on Fri Jul 19 16:42:20 2013
 
 import pandas as pd
 # import rows 1-4501 and add column titles
-traintemp = pd.read_csv('./input00.txt', sep="\s", skiprows=1,nrows=4500, names=["id","rank","col1","col2","col3","col4","col5","col6","col7","col8","col9","col10","col11","col12","col13","col14","col15","col16","col17","col18","col19","col20","col21","col22","col23"])
+train = pd.read_csv('./input00.txt', sep="\s", skiprows=1,nrows=4500, names=range(25))
 # import rows 4502-6002 and add column titles
-testtemp = pd.read_csv('./input00.txt', sep="\s", skiprows=4502, names=["id","col1","col2","col3","col4","col5","col6","col7","col8","col9","col10","col11","col12","col13","col14","col15","col16","col17","col18","col19","col20","col21","col22","col23"])
+test = pd.read_csv('./input00.txt', sep="\s", skiprows=4502, names=range(24))
 # for columns slice out col id and :
-# 1-9 remove "n:" 10-23 remove "nn:"
+# training data - first 9 features
+for col in range(2,11):
+   train[col] = train[col].map(lambda val: float(str(val)[2:]))
+# training data - first 9 features
+for col in range(11,25):
+   train[col] = train[col].map(lambda val: float(str(val)[3:]))
+# test data - first 9 features
+for col in range(1,10):
+   test[col] = test[col].map(lambda val: float(str(val)[2:]))
+# test data - first 9 features
+for col in range(10,24):
+   test[col] = test[col].map(lambda val: float(str(val)[3:]))
+
+# scale data
+# turn -1 to 0 and leave 1 as 1 - THIS WILL TURN ALL NON -1 VALS TO 1!!! check this
+train[1] = train[1].map(lambda val: 0 if val == -1 else 1)
+test[1] = test[1].map(lambda val: 0 if val == -1 else 1)
+# find max and divide each number by max
+
